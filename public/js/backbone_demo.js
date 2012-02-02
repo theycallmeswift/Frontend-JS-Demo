85261a976ed9c791796b5574841efeb4ceac3374
Mike Swift <theycallmeswift@gmail.com>
2012-02-02 17:41:17 -0500

    WIP: Started filling in Backbone App. Off to get some dinner tho yo.

--- public/js/backbone_demo.js
+++ public/js/backbone_demo.js
@@ -1,22 +1,83 @@
+// This will eventually hold the authenticated user model.
+var currentUser;
+
 $(document).ready(function() {
 
 
   /**
-   * Views
+   * The User Model
    */
+  var User = Backbone.Model.extend({});
 
-  // Sidebar View
-  //
-  // Contains the currently active users.
+  /**
+   * The User Collection
+   */
+  var UserList = Backbone.Collection.extend({
+    model: User
+  });
 
-  var SidebarView = Backbone.View.extend({
-    el: "div#sidebar",
+
+  /**
+   * The ChatLog View
+   */
+  var ChatLog = Backbone.View.extend({
+    // Attach the view to the #app #log div
+    el: $("#app #log"),
+
+    // Initialize the view
+    initialize: function() {
+      // Bind the following methods to the current context of this.
+      _.bindAll(this, 'render');
+
+      // This view is self rendering
+      this.render();
+    },
 
     render: function() {
-      var self = this;
-      $(self.el).css({'height':(($(window).height())-40)+'px'})
+      // Display the log which is hidden by default.
+      $(this.el).show();
     }
   });
 
-  window.Sidebar = new SidebarView();
+  var chatLog = new ChatLog();
+
+  /**
+  * The Signup View
+  */
+  var Signup = Backbone.View.extend({
+    // Attach the view to the signup form
+    el: $('#signup'),
+
+    // Attach 
+    events: {
+      'click #connectButton': 'signUp'
+    },
+
+    initialize: function() {
+      // Bind signUp to the current context of this
+      _.bindAll(this, 'signUp');
+    },
+
+    /**
+     * signUp
+     *
+     * Handles authenticating new users.
+     */
+     signUp: function(event) {
+       var self = this;
+
+       event.preventDefault();
+
+       var email = $('#email', self.el).val();
+       var nickname = $('#nickname', self.el).val();
+
+       currentUser = new User({ email: email, nickname: nickname });
+
+       $('#entry').show();
+       this.remove();
+     }
+
+  });
+
+  var signup = new Signup();
 });
